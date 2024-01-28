40e1c506ebac08a1acb17119576884b29cb39afb
jshuckbot <gorka_ivan@mail.ru>
2024-01-28 13:37:28 +0700

    ready lesson 5

---
+++ lesson_5/src/main/java/ru/gb/App.java
@@ -0,0 +1,14 @@
+package ru.gb;
+
+/**
+ * Lesson 5
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Table table = new Table();
+        table.start();
+    }
+}
